a2efe10b6bd9cd3182fd29e13e5ee64683953714
IvanPuchkov <ivan.puchkov1995@gmail.com>
2015-11-02 15:24:25 +0300

    Task2

---
+++ GCDAlgorithm/GCDAlgorithm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace GCDAlgorithm
+{
+    public static class GcdAlgorithm
+    {
+        private delegate int GcdAlgorithmDelegate(int a, int b);
+
+        private static int CalculateEucledeanGcd(int a, int b)
+        {
+            if ((a == 0) || (b == 0))
+            {
+                return Math.Max(a, b);
+            }
+            int output = a > b ? CalculateEucledeanGcd(a % b, b) : CalculateEucledeanGcd(a, b % a);
+            return output;
+        }
+
+        public static int CalculateSteinGcd(int a, int b)
+        {
+            if ((a == 0) && (b == 0))
+                return 0;
+            if ((a == 0) || (b == 0))
+                return Math.Max(a, b);
+            if ((a % 2 == 0) && (b % 2 == 0))
+                return CalculateSteinGcd(a / 2, b / 2) * 2;
+            if (a % 2 == 0)
+                return CalculateSteinGcd(a / 2, b);
+            if (b % 2 == 0)
+                return CalculateSteinGcd(a, b / 2);
+            int greatest = Math.Max(a, b);
+            int lowest = Math.Min(a, b);
+            return CalculateSteinGcd((greatest - lowest) / 2, lowest);
+        }
+
+        private static int Calculate(GcdAlgorithmDelegate algorithm,int a, int b)
+        {
+            if ((a == 0) || (b == 0))
+                throw new ArgumentException("None of numbers should be equal to zero");
+            return algorithm(a, b);
+        }
+
+        private static int Calculate(GcdAlgorithmDelegate algorithm, int a, int b, int c)
+            => Calculate(algorithm, a, Calculate(algorithm, b, c));
+
+        private static int Calculate(GcdAlgorithmDelegate algorithm, params int[] numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException();
+            if (numbers.Count() < 2)
+                throw new ArgumentException();
+            int output = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                output = Calculate(algorithm,output, numbers[i]);
+            }
+            return output;
+        }
+
+        private static int Calculate(GcdAlgorithmDelegate algorithm, out long time, int a, int b)
+        {
+            Stopwatch sw=new Stopwatch();
+            sw.Start();
+            int result=Calculate(algorithm, a, b);
+            sw.Stop();
+            time = sw.ElapsedMilliseconds;
+            return result;
+        }
+
+        private static int Calculate(GcdAlgorithmDelegate algorithm, out long time, int a, int b, int c)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int result = Calculate(algorithm, a, b, c);
+            sw.Stop();
+            time = sw.ElapsedMilliseconds;
+            return result;
+        }
+
+        private static int Calculate(GcdAlgorithmDelegate algorithm, out long time, params int[] numbers)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int result = Calculate(algorithm, numbers);
+            sw.Stop();
+            time = sw.ElapsedMilliseconds;
+            return result;
+        }
+
+        public static int EucledeanGcd(int a, int b) => Calculate(CalculateEucledeanGcd, a, b);
+
+        public static int EucledeanGcd(int a, int b, int c) => Calculate(CalculateEucledeanGcd, a, b, c);
+
+        public static int EucledeanGcd(params int[] numbers) => Calculate(CalculateEucledeanGcd, numbers);
+
+        public static int EucledeanGcd(out long time, int a, int b) => Calculate(CalculateEucledeanGcd, out time, a, b);
+
+        public static int EucledeanGcd(out long time, int a, int b, int c)
+            => Calculate(CalculateEucledeanGcd, out time, a, b, c);
+
+        public static int EucledeanGcd(out long time, params int[] numbers)
+            => Calculate(CalculateEucledeanGcd, out time, numbers);
+
+        public static int SteinGcd(int a, int b) => Calculate(CalculateSteinGcd, a, b);
+
+        public static int SteinGcd(int a, int b, int c) => Calculate(CalculateSteinGcd, a, b, c);
+
+        public static int SteinGcd(params int[] numbers) => Calculate(CalculateSteinGcd, numbers);
+
+        public static int SteinGcd(out long time, int a, int b) => Calculate(CalculateSteinGcd, out time, a, b);
+
+        public static int CalculateSteinGcd(out long time, int a, int b, int c)
+            => Calculate(CalculateSteinGcd, out time, a, b, c);
+
+        public static int SteinGcd(out long time, params int[] numbers) => Calculate(SteinGcd, out time, numbers);
+
+    }
+}
+
+    
---
+++ GcdAlgorithm.Tests/GcdAlgorithmTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace GcdAlgorithm.Tests
+{
+    [TestFixture]
+    public class GcdAlgorithmTests
+    {
+        [TestCase(10, 5, Result = 5)]
+        [TestCase(5, 1, Result = 1)]
+        [TestCase(964, 592, Result = 4)]
+        public int EuclideanAlgorithmTests_Two_Numbers(int a, int b)=>
+            GCDAlgorithm.GcdAlgorithm.EucledeanGcd(a, b);
+        
+
+        [TestCase(964, 592, 400, Result = 4)]
+        [TestCase(100, 5, 20, Result = 5)]
+        public int EuclideanAlgorithmTests_Three_Numbers(int a, int b, int c)
+            => GCDAlgorithm.GcdAlgorithm.EucledeanGcd(a, b, c);
+        
+
+
+        [TestCase(10, 20, 30, 40, Result = 10)]
+        public int EuclideanAlgorithmTests_Multiple_Numbers(params int[] numbers) =>
+            GCDAlgorithm.GcdAlgorithm.EucledeanGcd(numbers);
+        
+
+
+        [TestCase(10, 5, Result = 5)]
+        [TestCase(5, 1, Result = 1)]
+        [TestCase(964, 592, Result = 4)]
+        public int SteinAlgorithmTests_Two_Numbers(int a, int b) => GCDAlgorithm.GcdAlgorithm.SteinGcd(a, b);
+
+
+        [TestCase(964, 592, 400, Result = 4)]
+        [TestCase(100, 5, 20, Result = 5)]
+        public int SteinAlgorithmTests_Three_Numbers(int a, int b, int c) => GCDAlgorithm.GcdAlgorithm.SteinGcd(a, b, c);
+
+
+
+        [TestCase(10, 20, 30, 40, Result = 10)]
+        public int SteinAlgorithmTests_Multiple_Numbers(params int[] numbers)
+            => GCDAlgorithm.GcdAlgorithm.SteinGcd(numbers);
+
+    }
+}
